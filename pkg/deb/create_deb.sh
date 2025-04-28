bcd68c85530c0490b91fa3c4c1814d61c989c72c
heng30 <2238288979@qq.com>
2025-04-28 17:36:56 +0800

    [+] v0.1.2

--- pkg/deb/create_deb.sh
+++ pkg/deb/create_deb.sh
@@ -1,35 +1,28 @@
-#!/bin/bash
+#!/bin/sh
 
+app_name="flymoon"
 icon_name="brand.png"
 icon_dir="../../ui/images"
-dst_icon_name="flymoon.png"
-dst_icon_name_svg="flymoon.svg"
-
-cp ../../target/release/flymoon ./package/usr/local/bin/
-
+bin_dir="./package/usr/local/bin"
+dst_icon_name="${app_name}.png"
+dst_icon_name_svg="${app_name}.svg"
 sizes=(16x16 22x22 24x24 32x32 36x36 48x48 64x64 72x72 96x96 128x128 192x192 256x256 512x512)
+
+mkdir -p ${bin_dir}
+cp ../../target/release/${app_name} ${bin_dir}
+chmod a+x ${bin_dir}/${app_name}
 
 for size in "${sizes[@]}"; do
     mkdir -p ./package/usr/share/icons/hicolor/${size}/apps
     magick "${icon_dir}/${icon_name}" -resize "$size" -background none -gravity center -extent "$size" "./package/usr/share/icons/hicolor/${size}/apps/${dst_icon_name}"
 done
 
-# cp ../../ui/images/${icon_name} ./package/usr/share/icons/hicolor/symbolic/apps/${dst_icon_name_svg}
-# cp ../../ui/images/${icon_name} ./package/usr/share/icons/hicolor/scalable/apps/${dst_icon_name_svg}
+dpkg-deb --build package ${app_name}.deb
 
-chmod a+x ./package/usr/local/bin/flymoon
-
-dpkg-deb --build package
-
-rm -f ./package/usr/local/bin/flymoon
+rm -f ${bin_dir}/${app_name}
 
 for size in "${sizes[@]}"; do
     rm -f ./package/usr/share/icons/hicolor/${size}/apps/${dst_icon_name}
 done
-
-# rm -f ./package/usr/share/icons/hicolor/symbolic/apps/${dst_icon_name_svg}
-# rm -f ./package/usr/share/icons/hicolor/scalable/apps/${dst_icon_name_svg}
-
-mv -f package.deb flymoon.deb
 
 exit $?
